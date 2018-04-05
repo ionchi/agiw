6283df04b69b1ac312ed6041fbbc0a239e784edc
ionchi <ion.chiriac@live.com>
2018-04-05 19:54:19 +0200

    nuove specifiche

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Agiw homework
 
 ## Script use
 
-`./webpagesDownload.py site_list.json`
+`./webpagesDownload.py site_list.json category_name`
 
 ## Used libs (python 3)
 * `pip install beautifulsoup4`
 * `pip install requests`
+* `pip install lxml`
--- webpagesDownload.py
+++ webpagesDownload.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import os
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -7,8 +8,7 @@ from collections import OrderedDict
 
 
 def main():
-    # variabile per scandire i file scaricati
-    i = 1
+    folderpath = "./"+sys.argv[2]
     # apre il file passato come parametro allo script
     with open(sys.argv[1], 'r') as in_file:
         # orderDict per preservare l'ordine del json
@@ -16,10 +16,16 @@ def main():
 
         # itera le chiavi del json
         for key, value in data.items():
+            # variabile per scandire i file scaricati
+            i = 1
             # itera i valori (in questo caso url)
+            sitepath = os.path.join(folderpath, key)
+            if not os.path.exists(sitepath):
+                os.makedirs(sitepath)
             for url in value:
                 # nome file output
-                output = "file_" + str(i) + ".html"
+                output = os.path.join(sitepath, str(i) + ".html")
+                resultfilepath = os.path.join(sitepath, "index.txt")
                 # rende gli url nel giusto formato
                 url = url.rstrip()
                 try:
@@ -28,24 +34,24 @@ def main():
                     response.raise_for_status()
                     # scaricare solo i siti con codice 200
                     if response.status_code == 200:
-                        with open("results.txt", "a") as resultfile:
-                            resultfile.write(url + " -> " + output + "\n")
+                        with open(resultfilepath, "a") as resultfile:
+                            resultfile.write(url + " \t " + output + "\n")
                         # Download contenuto risposta http
                         soup = BeautifulSoup(response.content, "lxml")
                         # salvataggio file html (opzionale: prettify)
                         with open(output, 'a') as fp:
                             fp.write(soup.prettify())
                 except requests.exceptions.HTTPError:
-                    with open("results.txt", "a") as resultfile:
-                        resultfile.write(url + " -> " + str(response.status_code)+"\n")
+                    with open(resultfilepath, "a") as resultfile:
+                        resultfile.write(url + " \t" + str(response.status_code)+"\n")
                     pass
                 except requests.exceptions.RequestException as e:
                     data = str(e)
                     # eccezione diversa dai codici http
                     # accorciata a 25 caratteri sul file di output
                     info = data[:25] + (data[25:] and '..')
-                    with open("results.txt", "a") as resultfile:
-                        resultfile.write(url + " -> other error: " + info+"\n")
+                    with open(resultfilepath, "a") as resultfile:
+                        resultfile.write(url + " \t other error: " + info+"\n")
                     pass
                 i = i + 1
 
